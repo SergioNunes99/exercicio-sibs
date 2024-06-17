aeffdf36a28480ea96dae28e75df5a6259e51613
Sergio Nunes <sergionunesricardonunes@gmail.com>
2024-06-17 21:04:43 +0100

    first commit

---
+++ src/main/java/com/example/exercicio_sibs/ExercicioSibsApplication.java
@@ -0,0 +1,13 @@
+package com.example.exercicio_sibs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExercicioSibsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExercicioSibsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/exercicio_sibs/ExercicioSibsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exercicio_sibs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExercicioSibsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
